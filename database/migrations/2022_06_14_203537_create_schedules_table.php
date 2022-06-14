b5ff9ebaa92e5419f49a9cd5a6652ce0b2db69b0
david1066 <davinchi1066@hotmail.com>
2022-06-14 16:15:22 -0500

    creacion de migraciones,modelos y controladores

---
+++ database/migrations/2022_06_14_203537_create_schedules_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSchedulesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('schedules', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('lesson_id');
+            $table->smallInteger('weekday');
+            $table->time('time_start');
+            $table->time('time_end');
+            $table->timestamps();
+            $table->foreign('lesson_id')->references('id')->on('lessons');
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('schedules');
+    }
+}
